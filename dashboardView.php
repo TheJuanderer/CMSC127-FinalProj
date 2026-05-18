61be1a4ec05139aff27be2888630b5e703da3371
TheJuanderer <brentmichaelmendozaup@gmail.com>
2026-05-19 05:04:50 +0800

    renamed dashboard into dashboardView, added selections for searching reports

---
+++ dashboardView.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+    </head>
+    <body>
+        <h2>This is the dashboard!</h2>
+        <select> name = "statusSearch"
+            <option value = "OPEN"></option>
+            <option value = "CLOSED"></option>
+            <option value = "ALL"></option>
+        </select>
+    </body>
+
+</html>
